a440afc453fb6be54f9bb84024a0961273e2d2b6
Alexandr Kurilin <alex@kurilin.net>
2013-10-27 19:00:31 -0700

    Add gitconfig soft link

--- setup-link.sh
+++ setup-link.sh
@@ -4,6 +4,7 @@ ln -f -s `pwd`/.bashrc.extras ~/.bashrc.extras
 ln -f -s `pwd`/.tmux.conf ~/.tmux.conf
 ln -f -s `pwd`/.vimrc ~/.vimrc
 ln -s `pwd`/.vimrc.vimwiki ~/.vimrc.vimwiki
+ln -f -s `pwd`/.gitconfig ~/.gitconfig
 
 
 ln -f -s `pwd`/ftplugin -t ~/.vim ftplugin
