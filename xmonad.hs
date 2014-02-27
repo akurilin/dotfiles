a0d966fbe2d060b32f4d27aa8670920350d194eb
Alexandr Kurilin <alex@kurilin.net>
2014-02-26 18:24:22 -0800

    Update xmonad to have xmodmap example

--- xmonad.hs
+++ xmonad.hs
@@ -1,6 +1,9 @@
 import XMonad
+import XMonad.Util.SpawnOnce
 
 main = xmonad defaultConfig
          { modMask = mod4Mask
-         -- , terminal = "urxvt"
+         -- enable this when you want to run some xmodmap commands to replace
+         -- key bindings
+         -- , startupHook = spawnOnce "xmodmap ~/.xmodmap"
          }
