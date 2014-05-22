125dec3da1944575053c35d4571fa3e83b36ecfc
Alexandr Kurilin <alex@kurilin.net>
2014-05-21 19:11:46 -0700

    Add .ghci

--- setup-link.sh
+++ setup-link.sh
@@ -4,6 +4,7 @@ ln -f -s `pwd`/.bashrc.extras ~/.bashrc.extras
 ln -f -s `pwd`/.tmux.conf ~/.tmux.conf
 ln -f -s `pwd`/.vimrc ~/.vimrc
 ln -f -s `pwd`/.gemrc ~/.gemrc
+ln -f -s `pwd`/.ghci ~/.ghci
 if [ ! -f ~/.vimrc.vimwiki ]
 then
   ln -s `pwd`/.vimrc.vimwiki ~/.vimrc.vimwiki
